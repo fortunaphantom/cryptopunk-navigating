91c6f121605b3f534e45ced09266814bcfc337a6
unknown <fortuna.star07@gmail.com>
2022-09-19 09:08:15 -0400

    add footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,3 +1,36 @@
+import { Container } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopyright } from '@fortawesome/free-regular-svg-icons'
+
 export default function Footer() {
-  return <h1>Footer</h1>;
+  return (
+    <footer
+      style={{
+        width: '100%',
+        position: 'fixed',
+        bottom: '0',
+        backgroundColor: "#ea34b0",
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: '14px',
+        zIndex: '5'
+      }}
+    >
+      <Container
+        maxW={{
+          sm: "container.sm",
+          md: "container.md",
+          lg: "container.lg",
+          xl: "container.xl",
+        }}
+        paddingTop={"8px"}
+        paddingBottom={"8px"}
+        margin='auto'
+      >
+        
+        <FontAwesomeIcon icon={faCopyright} />
+        {' '}2022 CryptoPunks USA, Inc. All rights reserved.
+      </Container>
+    </footer>
+  );
 }
